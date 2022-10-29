7f7a13a19ebaf5ca67f69393e5c937310be84b09
Veronika Kravchenko <aniregma@gmail.com>
2022-10-29 12:39:04 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,18 +4,17 @@ import { Section } from './Section/Section';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 
-const feedbackList = ['good', 'neutral', 'bad'];
-
 export function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const feedbackList = ['good', 'neutral', 'bad'];
   const total = good + neutral + bad;
   const positive = Math.round((good * 100) / total);
 
   const handleClick = e => {
-    const name = e.target.name;
+    const { name } = e.target;
     switch (name) {
       case 'good':
         setGood(state => state + 1);
